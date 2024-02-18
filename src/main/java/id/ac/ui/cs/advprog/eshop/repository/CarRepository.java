18de6e83d75dc14ec8920df9d9b987a71ac85c69
sorfeb <soros21febriano@gmail.com>
2024-02-19 00:31:05 +0700

    Commit CarService.java and CarServiceImpl.java to your Git repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -10,7 +10,7 @@ public class CarRepository {
 
     private List<Car> carData = new ArrayList<>();
 
-    public Car createCar(Car car) {
+    public Car create(Car car) {
         if (car.getCarId() == null) {
             UUID uuid = UUID.randomUUID();
             car.setCarId(uuid.toString());
